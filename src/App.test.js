eedfd90c1d3e917b0f9082456e35cce6d4e0fd41
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-03-27 20:11:24 +0100

    test for into text

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders explanation to the user', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Wordmap creation just got easier.../);
   expect(linkElement).toBeInTheDocument();
